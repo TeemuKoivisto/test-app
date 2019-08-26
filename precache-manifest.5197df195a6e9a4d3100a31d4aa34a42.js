d008efa92f1040cadb95a92f0a434f7471236f87
Teemu Koivisto <tkoivisto456@gmail.com>
2019-08-26 14:36:39 +0300

    Updates

---
+++ precache-manifest.5197df195a6e9a4d3100a31d4aa34a42.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b106db7238256a19af6b1e0fa94f699",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "1bb34dd6f04c9b3ac729",
+    "url": "/test-app/static/js/2.d1d0d36f.chunk.js"
+  },
+  {
+    "revision": "17ca6d4dc023f08fba30",
+    "url": "/test-app/static/js/main.4a1a7d17.chunk.js"
+  },
+  {
+    "revision": "7318f3809038590af7ec",
+    "url": "/test-app/static/js/runtime~main.e9adfd5a.js"
+  }
+]);
